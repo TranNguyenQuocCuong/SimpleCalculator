4aa005a22b0fb5b41b581150b2b23497434b75bd
TranNguyenQuocCuong <cuongtnqde170165@fpt.edu.vn>
2024-05-17 23:18:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity1.java
@@ -0,0 +1,116 @@
+package com.example.simplecalculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    public String snum1;
+    public String snum2;
+    public double dnum1;
+    public double dnum2;
+    public double dresult;
+    public EditText num1;
+    public EditText num2;
+    public Button add;
+    public Button sub;
+    public Button mul;
+    public Button div;
+    public EditText result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        add = findViewById(R.id.add);
+        sub = findViewById(R.id.sub);
+        mul = findViewById(R.id.mul);
+        div = findViewById(R.id.div);
+        result = findViewById(R.id.result);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snum1 = num1.getText().toString();
+                snum2 = num2.getText().toString();
+                try {
+                    dnum1 = Double.parseDouble(snum1);
+                    dnum2 = Double.parseDouble(snum2);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Invalid input", Toast.LENGTH_SHORT).show();
+                }
+                dresult = dnum1 + dnum2;
+                result.setText(String.valueOf(Math.round(dresult * 100.0) / 100.0));
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snum1 = num1.getText().toString();
+                snum2 = num2.getText().toString();
+                try {
+                    dnum1 = Double.parseDouble(snum1);
+                    dnum2 = Double.parseDouble(snum2);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Invalid input", Toast.LENGTH_SHORT).show();
+                }
+                dresult = dnum1 - dnum2;
+                result.setText(String.valueOf(Math.round(dresult * 100.0) / 100.0));
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snum1 = num1.getText().toString();
+                snum2 = num2.getText().toString();
+                try {
+                    dnum1 = Double.parseDouble(snum1);
+                    dnum2 = Double.parseDouble(snum2);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Invalid input", Toast.LENGTH_SHORT).show();
+                }
+                dresult = dnum1 * dnum2;
+                result.setText(String.valueOf(Math.round(dresult * 100.0) / 100.0));
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snum1 = num1.getText().toString();
+                snum2 = num2.getText().toString();
+                try {
+                    dnum1 = Double.parseDouble(snum1);
+                    dnum2 = Double.parseDouble(snum2);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Invalid input", Toast.LENGTH_SHORT).show();
+                }
+                if (dnum2 == 0) {
+                    Toast.makeText(getApplicationContext(), "Cannot divide by 0", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                dresult = dnum1 / dnum2;
+                result.setText(String.valueOf(Math.round(dresult * 100.0) / 100.0));
+            }
+        });
+    }
+}
